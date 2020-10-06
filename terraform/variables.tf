387974eb07ceef3876fa5c74bef7b0029882a26c
andrii <andriim.ua@gmail.com>
2020-10-06 12:42:15 +0200

    Deploying resources with terraform to GCP

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "project" { default = "terraform-course-291611" }
+
+variable "region" { default = "us-central1" }
+
+variable "zone" { default = "us-central1-c" }
+
+variable "cidr" { default = "10.0.0.0/16" }
+
